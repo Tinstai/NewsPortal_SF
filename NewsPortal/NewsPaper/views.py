c2a534aa4ebf10f435809e029b2be6545f18a3e0
Tinstai <130226070+Tinstai@users.noreply.github.com>
2023-06-09 01:36:26 +0300

    project

---
+++ NewsPortal/NewsPaper/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from django.views.generic import ListView, DetailView
+
+from .models import Post
+
+
+class PostsList(ListView):
+    model = Post
+
+    template_name = 'news.html'
+
+    context_object_name = 'news'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
